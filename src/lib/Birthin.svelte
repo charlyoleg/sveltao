6d422b56867805736089b88bb3bd203de737f24e
charles <charles.braquet@e.email>
2022-05-29 22:52:12 +0200

    first component

---
+++ src/lib/Birthin.svelte
@@ -0,0 +1,11 @@
+<div>
+	<h2>Enter your year of birth</h2>
+</div>
+
+<style>
+
+	div h2 {
+		color: green;
+	}
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+<script>
+
+	import Birthin from '$lib/Birthin.svelte';
+
+</script>
+
 <h1>Page Home</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Birthin/>
+
