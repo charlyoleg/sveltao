2db730178da13f416764942b4c47339dd9b076cf
charles <charles.braquet@e.email>
2022-05-29 22:41:51 +0200

    3 pages

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Ma SPA</title>
+</svelte:head>
+
+
+<nav>
+	<a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/">Home</a>
+	<a class:active={$page.url.pathname === '/page2'} sveltekit:prefetch href="/page2">Page2</a>
+	<a class:active={$page.url.pathname === '/page3'} sveltekit:prefetch href="/page3">Page3</a>
+</nav>
+<slot></slot>
+
+<style>
+	a {
+		color: blue;
+	}
+
+	a.active {
+		color: orange;
+	}
+</style>
