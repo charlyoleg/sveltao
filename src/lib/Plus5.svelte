32113b3c80aa927013348becbe8177c8a6cb9bd9
charles <charles.braquet@e.email>
2022-05-29 23:04:39 +0200

    adding component Plus5

---
+++ src/lib/Plus5.svelte
@@ -0,0 +1,19 @@
+<script>
+	let abc = 0;
+</script>
+
+
+<section>
+	<h3>Plus 5</h3>
+	<input type="number" bind:value={abc}>
+	<p>+ 5 = {abc+5}</p>
+</section>
+
+
+<style>
+
+	section h3 {
+		color: grey;
+	}
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script>
 
 	import Birthin from '$lib/Birthin.svelte';
+	import Plus5 from '$lib/Plus5.svelte';
 
 </script>
 
 <h1>Page Home</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <Birthin/>
+<Plus5/>
 
