354f9923b4336dfb86f9c98fc60560d3b6542a96
charles <charles.braquet@e.email>
2022-05-30 20:11:35 +0200

    trying multiadpater

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,33 @@
 //import adapter from '@sveltejs/adapter-auto';
 //import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-node';
+import staticAdapter from '@sveltejs/adapter-static';
+import nodeAdapter from '@sveltejs/adapter-node';
+import multiAdapter from '@macfja/svelte-multi-adapter';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 
 	kit: {
-		adapter: adapter({out: 'build'}),
+		//adapter: adapter({out: 'build'}),
 		//adapter: adapter({
 		//	pages: 'build',
 		//	assets: 'build',
 		//	fallback: 'index.html',
 		//	precompress: false
 		//}),
+		adapter: multiAdapter([
+			staticAdapter({
+				pages: 'output_static',
+				assets: 'output_static',
+				fallback: 'index.html',
+				precompress: false
+			}),
+			nodeAdapter({
+				out: 'output_node'
+			})
+		]),
 		trailingSlash: 'always'
 	},
 };
