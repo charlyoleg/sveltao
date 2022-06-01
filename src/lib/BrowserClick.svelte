ee41c06f7479aab99d6c6c6f813ce0b8beae4239
charles <charles.braquet@e.email>
2022-06-01 11:50:48 +0200

    finding a solution for onMount and on:click

--- src/lib/BrowserClick.svelte
+++ src/lib/BrowserClick.svelte
@@ -1,9 +1,46 @@
 <script>
 
+	import { onMount } from 'svelte';
+	//import { browser } from '$app/env';
+
+
 	let count = 50;
+	let elem_button;
+
+	function getElemButton2(node) {
+		elem_button = node;
+	}
+
+	onMount( () => {
+
+		//function abc(x) {
+		//	return(x+2);
+		//}
+
+		function plusdix() {
+			count += 10;
+			//console.debug(elem_button);
+		}
+
+		elem_button.addEventListener("click", plusdix);
+		//document.addEventListener("click", plusdix);
+
+		return( () => {
+			//if (!elem_button) {
+				elem_button.removeEventListener("click", plusdix);
+			//}
+		});
+	});
+
+
 
 	function plusdeux() {
-		count += 2;
+		//if (browser) {
+			//count = abc(count);
+			count += 2;
+		//} else {
+		//	console.log("Not in browser");
+		//}
 	}
 
 </script>
@@ -11,6 +48,7 @@
 <section>
 	<h3>Le composant BrowserClick</h3>
 	<button on:click={plusdeux}>Action</button>
+	<button use:getElemButton2>Action2</button>
 	<p>le message est {count}</p>
 
 </section>
