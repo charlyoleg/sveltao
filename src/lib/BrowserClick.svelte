bcde819904ed3f95747d1951fc69c64d56d5441b
charles <charles.braquet@e.email>
2022-06-01 14:54:15 +0200

    Better implementation of button action

--- src/lib/BrowserClick.svelte
+++ src/lib/BrowserClick.svelte
@@ -5,6 +5,7 @@
 
 	let count = 50;
 	let elem_button;
+	let fPlusCent = () => { count += 100; };
 
 	function getElemButton2(node) {
 		elem_button = node;
@@ -17,6 +18,9 @@
 		}
 
 		elem_button.addEventListener("click", plusdix);
+
+		const fPlusMille = () => { count += 1000; };
+		fPlusCent = fPlusMille;
 
 		return( () => {
 			elem_button.removeEventListener("click", plusdix);
@@ -35,6 +39,7 @@
 	<h3>Le composant BrowserClick</h3>
 	<button on:click={plusdeux}>Action</button>
 	<button use:getElemButton2>Action2</button>
+	<button on:click={fPlusCent}>Action3</button>
 	<p>le message est {count}</p>
 </section>
 
